b5aea1c22d894bd74d2d2d7f032f30efdad2bc48
Lokesh-ramesh <95431053+Lokesh-ramesh@users.noreply.github.com>
2023-06-15 19:06:43 +0530

    remove node modules

--- bundle.js
+++
@@ -1,7 +0,0 @@
-const firebase = require("firebase/app");
-require("firebase/database");
-
-const getDatabase = firebase.getDatabase;
-const ref = firebase.ref;
-const child = firebase.child;
-const get = firebase.get;
